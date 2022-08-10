6d932558db9eff2a8f05c0bb0b77da1d7d3fff56
thetzaw202208 <110457134+thetzaw202208@users.noreply.github.com>
2022-08-10 09:46:14 +0630

    Delete lib directory

--- lib/utils/color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const blue = Color.fromARGB(255, 0, 152, 212);
-const black = Color.fromARGB(255, 55, 57, 59);
-const grey = Color.fromARGB(255, 138, 134, 134);
-const colorerror = Color.fromARGB(255, 168, 8, 8);
-const coloraccept = Color.fromARGB(255, 2, 136, 62);
-const maincolor = Color.fromARGB(255, 13, 152, 186);
--- lib/utils/font.dart
+++
@@ -1,2 +0,0 @@
-// ignore: constant_identifier_names
-const custom_font = 'Nunito';
